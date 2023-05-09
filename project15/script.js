77cbf3afe526b19b02a5340d16a44447f823b5cc
ArsenLen <laurie10@mail.ru>
2023-05-09 14:20:20 +0600

    product card styles

--- project15/script.js
+++ project15/script.js
@@ -19,11 +19,19 @@ form.addEventListener("submit", function(event) {
     event.preventDefault() // отменяет действия события по умолчанию
     
     const newProduct = {
-        name: productName.value,
-        price: Number(productPrice.value)
+        name: productName.value, // данные введенные в инпут Name
+        price: Number(productPrice.value) // данные введеные в инпут Price
     }
-    products.push(newProduct)
+    products.push(newProduct) // добавляем объект newProduct в массив products
+
+    clearInputs() // функция очистки полей ввода 
 })
+
+function clearInputs() {
+    productName.value = ""
+    productPrice.value = ""
+}
+
 
 /*
     submit - событие отправки формы
